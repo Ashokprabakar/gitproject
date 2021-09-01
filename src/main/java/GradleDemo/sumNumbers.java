a5840cd77bd0e75e78a06fb2642bfeb28775e4de
Ashokprabakar <vkpashok@gmail.com>
2021-09-01 12:24:02 +0530

    Sum numbers changed

--- src/main/java/GradleDemo/sumNumbers.java
+++ src/main/java/GradleDemo/sumNumbers.java
@@ -2,7 +2,7 @@ package GradleDemo;
 
 public class sumNumbers {
 	public int sum(int a, int b) {
-		if (a>20) {
+		if (a>10) {
 			throw new IllegalArgumentException("Value should be less than 10");
 		}
 		return a+b;
